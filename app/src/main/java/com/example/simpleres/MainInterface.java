72b474811bb36b98862ef0d4ab8a429bae602916
rickp3ns3 <48182450+rickp3ns3@users.noreply.github.com>
2019-09-27 14:02:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simpleres/MainInterface.java
@@ -0,0 +1,14 @@
+package com.example.simpleres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_interface);
+    }
+}
