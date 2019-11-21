25bc566d576e691c587fd4e309a8e683cb64a540
Bella Pense <apense@wayne.edu>
2019-11-21 15:08:13 -0500

    Resized the pop-up windows

--- app/src/main/java/com/example/simpleres/FutureDatePopup.java
+++ app/src/main/java/com/example/simpleres/FutureDatePopup.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.concurrent.Future;
 
 import static com.example.simpleres.MainInterface.isfinished;
 
@@ -37,25 +38,25 @@ public class FutureDatePopup extends AppCompatActivity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*0.3), (int)(height*0.8));
+        getWindow().setLayout((int)(width*0.8), (int)(height*0.8));
 
         //fields
-        final TextView displayDate = findViewById(R.id.display_date);
+        final TextView displayDate = findViewById(R.id.displayFutureDate);
         final ImageButton closePopup = findViewById(R.id.closeFuturePopup);
         //anything for the reservation list - Will be just like it is in Main Interface, just with no waitlist parties
 
         //PASSED EXTRA containing the date selected in form YYYY-MM-DD
         String dateSelected = getIntent().getStringExtra("DATE_SELECTED");
 
-        //reformat dateSelect to display for example November 14, 2019 for 2019-11-14
+        //reformat dateSelected to display for example November 14, 2019 for 2019-11-14
         //store values in following variables
         String month = "Month";
         String day = "Day";
         String year = "Year";
 
+        //display the formatted date
         String formattedDate = month + " " + day + ", " + year;
-
-        //displayDate.setText(month); //causing app to crash for some reason?
+        displayDate.setText(formattedDate);
 
         //Array of elements in the Future listview
         FutureList = (ListView) findViewById(R.id.FutureList);
@@ -74,7 +75,6 @@ public class FutureDatePopup extends AppCompatActivity {
                 PopupMenu FutureListActionMenu = new PopupMenu(view.getContext(), view);
                 FutureListActionMenu.getMenuInflater().inflate(R.menu.future_action_menu, FutureListActionMenu.getMenu());
                 final int pos = position;
-                //just need to fix where the menu pops up
 
                 FutureListActionMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
@@ -93,8 +93,6 @@ public class FutureDatePopup extends AppCompatActivity {
                                 Intent viewRes = new Intent(getApplicationContext(), ViewReservationPopup.class);
                                 viewRes.putExtra("DB_ID", dbId); //pass database ID for selected entry to the activity
                                 startActivityForResult(viewRes, isfinished);
-                                //startActivity(viewRes);
-                                //wdb.updateWaitlistEntry(selectedEntry);
                                 recreate();
                                 break;
                             case "Cancel":
@@ -122,9 +120,6 @@ public class FutureDatePopup extends AppCompatActivity {
                 }
             }
         };
-
-        //Just like in main interface, listen for when a party is tapped and open menu
-            //menu will only have 'Cancel' or 'View' options -> use future_party_action_menu instead
 
         closePopup.setOnClickListener(listener);
     }
